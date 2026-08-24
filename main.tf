fbcd2a3f0d33712ae176e3e34137f3d25d7cd728
Anderson Olson <olsonac@uci.edu>
2026-08-23 20:22:29 -0700

    removed asg and template startup for the free set up

--- main.tf
+++ main.tf
@@ -77,49 +77,7 @@ resource "aws_security_group" "matchtracker" {
     tags = { Name = "matchtracker-sg", Environment = var.environment }
 }
 
-# NON-FREE VERSION WITH ASG, WOULD NEED ALB TOO
-# # ---EC2 INSTANCE BLUEPRINT---
-# resource "aws_launch_template" "matchtracker" {
-#     name_prefix            = "matchtracker-lt-"
-#     image_id               = data.aws_ami.amazon_linux.id
-#     instance_type          = "t2.micro"
-#     vpc_security_group_ids = [aws_security_group.matchtracker.id]
-#     key_name               = var.key_name
 
-#     user_data = base64encode(templatefile("${path.module}/userdata.sh.tpl", 
-#                 {
-#                     flask_code = file("${path.module}/app/main.py")
-#                 }))
-    
-#     tag_specifications {
-#         resource_type = "instance"
-#         tags = { Name = "matchtracker-server", Environment = var.environment }
-#     }
-# }
-
-# # ---ASG (Auto Scaling Group)---
-# resource "aws_autoscaling_group" "matchtracker" {
-#     name             = "matchtracker-asg-${var.environment}"
-#     desired_capacity = var.instance_count
-#     min_size         = 1
-#     max_size         = 3
-#     availability_zones = ["${var.aws_region}a"]
-
-#     launch_template {
-#         id      = aws_launch_template.matchtracker.id
-#         version = "$Latest"
-#     }
-
-#     health_check_type         = "EC2"
-#     health_check_grace_period = 120
-
-#     tag { 
-#         key = "Name"
-#         value = "matchtracker-server"
-#         propagate_at_launch = true 
-#     }
-
-# }
 
 # ---EC2 Instance---
 resource "aws_instance" "matchtracker" {
@@ -152,9 +110,27 @@ resource "aws_eip" "matchtracker" {
 
 
 
-output "elastic_ip"       { value = aws_eip.matchtracker.public_ip }
-output "app_url"          { value = "http://${aws_eip.matchtracker.public_ip}" }
-output "health_url"       { value = "http://${aws_eip.matchtracker.public_ip}/health" }
-output "ssh_command"      { value = "ssh -i ~/.ssh/matchtracker-key.pem ec2-user@${aws_eip.matchtracker.public_ip}" }
-output "hosts_file_entry" { value = "${aws_eip.matchtracker.public_ip}  matchtracker.local" }
-output "asg_name"         { value = aws_autoscaling_group.matchtracker.name }
+output "instance_public_ips" {
+  value = aws_eip.matchtracker[*].public_ip
+}
+
+output "app_urls" {
+  value = [
+    for ip in aws_eip.matchtracker[*].public_ip :
+    "http://${ip}"
+  ]
+}
+
+output "health_urls" {
+  value = [
+    for ip in aws_eip.matchtracker[*].public_ip :
+    "http://${ip}/health"
+  ]
+}
+
+output "ssh_commands" {
+  value = [
+    for ip in aws_eip.matchtracker[*].public_ip :
+    "ssh -i ~/.ssh/matchtracker-key.pem ec2-user@${ip}"
+  ]
+}
